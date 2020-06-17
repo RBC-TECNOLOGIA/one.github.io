720fa54e4e7b7c0a40f453c70dfbe17503103c3d
RBC TECNOLOGIA <54333153+RBC-TECNOLOGIA@users.noreply.github.com>
2020-06-17 16:50:17 -0300

    at

---
+++ forms-forms-module.js
@@ -0,0 +1,59 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["forms-forms-module"],{
+
+/***/ "./src/app/pages/forms/forms.module.ts":
+/*!*********************************************!*\
+  !*** ./src/app/pages/forms/forms.module.ts ***!
+  \*********************************************/
+/*! exports provided: FormsModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "FormsModule", function() { return FormsModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _nebular_theme__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @nebular/theme */ "./node_modules/@nebular/theme/fesm5/index.js");
+/* harmony import */ var _theme_theme_module__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../../@theme/theme.module */ "./src/app/@theme/theme.module.ts");
+/* harmony import */ var _forms_routing_module__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./forms-routing.module */ "./src/app/pages/forms/forms-routing.module.ts");
+
+
+
+
+
+var FormsModule = /** @class */ (function () {
+    function FormsModule() {
+    }
+    FormsModule = Object(tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"])([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+            imports: [
+                _theme_theme_module__WEBPACK_IMPORTED_MODULE_3__["ThemeModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbInputModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbCardModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbButtonModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbActionsModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbUserModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbCheckboxModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbRadioModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbDatepickerModule"],
+                _forms_routing_module__WEBPACK_IMPORTED_MODULE_4__["FormsRoutingModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbSelectModule"],
+                _nebular_theme__WEBPACK_IMPORTED_MODULE_2__["NbIconModule"],
+            ],
+            declarations: [
+            //FormsComponent,
+            //ButtonsComponent,
+            //FormInputsComponent,
+            //FormLayoutsComponent,
+            //DatepickerComponent,
+            ],
+        })
+    ], FormsModule);
+    return FormsModule;
+}());
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=forms-forms-module.js.map
